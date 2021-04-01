5c23ea6d50dcb79bc985758025661e8155eb1281
elives100 <e.liv2know@gmail.com>
2021-04-01 04:11:45 -0400

    new branch with heroku links

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import onlyInt from "vue-input-only-number";
 import Panels from "./components/panel.vue";
 
+Vue.use(onlyInt);
 Vue.component("panel", Panels);
 
 Vue.config.productionTip = false;
